e5fe8be376974f079d2dd83e7a36674175f1857b
Jody Mardika <jody_mardika_24rpl@student.smktelkom-mlg.sch.id>
2017-08-23 15:28:37 +0700

    initial commit

---
+++ app/src/main/java/jody_mardika_xiirpl5_24rpl/webview/MainActivity.java
@@ -0,0 +1,33 @@
+package jody_mardika_xiirpl5_24rpl.webview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://simfoni.mitratel.co.id:8091/app/");
+    }
+}
